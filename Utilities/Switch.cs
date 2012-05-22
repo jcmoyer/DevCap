970176b471779fdd05337894fb32ba6fdbc27208
J.C. Moyer <jmoyer1992@gmail.com>
2012-05-22 16:02:01 -0400

    Adding disclaimer

--- Utilities/Switch.cs
+++ Utilities/Switch.cs
@@ -1,4 +1,10 @@
-﻿namespace DevCap.Utilities {
+﻿/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
+namespace DevCap.Utilities {
     class Switch {
         private readonly string _name;
         private readonly string _value;
--- Utilities/TaskQueue.cs
+++ Utilities/TaskQueue.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
+using System.Collections.Generic;
 using System.Threading;
 
 namespace DevCap.Utilities {
